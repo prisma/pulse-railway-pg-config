c13e3d86994a8c386d9fc591409d6e63222f984b
Marc Hess <marc@marchess.dev>
2023-07-31 16:01:56 -0400

    added logical check so the db isnt re-configured after every build if the user forgets to update the command and rmove the config folder

--- config/config-db.ts
+++ config/config-db.ts
@@ -12,13 +12,17 @@ async function main() {
 
 	try {
 		const db = await pool.connect();
-		await db.query("DROP EXTENSION timescaledb");
-		await db.query("ALTER SYSTEM SET wal_level = logical");
-		await db.query("ALTER SYSTEM SET max_replication_slots = 20");
-		await db.query("ALTER SYSTEM SET wal_keep_size = 2048");
-		await db.query("SELECT pg_reload_conf()");
-
-		console.log("All done please restart the database");
+		const wal = await db.query("SHOW wal_level");
+		if (wal.rows[0].wal_level != "logical") {
+			await db.query("DROP EXTENSION timescaledb");
+			await db.query("ALTER SYSTEM SET wal_level = logical");
+			await db.query("ALTER SYSTEM SET max_replication_slots = 20");
+			await db.query("ALTER SYSTEM SET wal_keep_size = 2048");
+			await db.query("SELECT pg_reload_conf()");
+			console.log("All done please restart the database");
+			return;
+		}
+		console.log("DB is already configured");
 	} catch (err) {
 		console.log(err);
 	}
